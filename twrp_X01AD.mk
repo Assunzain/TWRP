128e5177d7c7745549580e54189f44cd8a40e4ba
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-06-20 17:58:23 +0700

    Update twrp_X01AD.mk

--- twrp_X01AD.mk
+++ twrp_X01AD.mk
@@ -20,8 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/batik/config/common.mk)
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+
 
 # Charger
 PRODUCT_PACKAGES += \
