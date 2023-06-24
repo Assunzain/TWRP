ff250ed42f1ee9f83317713b82b78c9262128645
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-06-24 13:12:23 +0700

    X

--- twrp_X01AD.mk
+++ twrp_X01AD.mk
@@ -15,11 +15,11 @@
 #
 
 # Inherit Target Product stuff
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+
 
 # Charger
 PRODUCT_PACKAGES += \
